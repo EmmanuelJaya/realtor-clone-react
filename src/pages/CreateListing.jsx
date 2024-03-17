e2679c1d056e720395803f1887a94a0d4463efcc
EmmanuelJaya <emmanuelsk04@gmail.com>
2024-03-17 11:57:04 +0530

    created the listing page part-2

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -6,11 +6,16 @@ export default function CreateListing() {
         type: "rent",
         name: "",
         bedrooms: 1,
-        bathroom: 1,
+        bathrooms: 1,
         parking: false,
         furnished: false,
+        address: "",
+        description: "",
+        offer: false,
+        regularPrice: 0,
+        discountedPrice: 0,
     })
-    const {type, name, bedrooms, bathrooms, parking, furnished} = formData;
+    const {type, name, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountedPrice} = formData;
     function onChange(){}
   return (
     <main className="max-w-md px-2 mx-auto">
@@ -32,11 +37,11 @@ export default function CreateListing() {
             <div className="flex space-x-6 mb-6">
                 <div>
                     <p className="text-lg font-semibold">Beds</p>
-                    <input type="number" id="bedrooms" value={bedrooms} onChange={onChange} min="1" max="50" required className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:bprder-slate-600 text-center"></input>
+                    <input type="number" id="bedrooms" value={bedrooms} onChange={onChange} min="1" max="50" required className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"></input>
                 </div>
                 <div>
                 <p className="text-lg font-semibold">Baths</p>
-                    <input type="number" id="bathrooms" value={bathrooms} onChange={onChange} min="1" max="50" required className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:bprder-slate-600 text-center"></input>
+                    <input type="number" id="bathrooms" value={bathrooms} onChange={onChange} min="1" max="50" required className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"></input>
                 </div>
             </div>
             <p className="text-lg mt-6 font-semibold">Parking spot</p>
@@ -61,6 +66,55 @@ export default function CreateListing() {
                         no
                 </button>
             </div>
+            <p className="text-lg font-semibold">Address</p>
+            <textarea type="text" id="address" value={address} onChange={onchange} placeholder="Address" required className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600" />
+            <p className="text-lg font-semibold">Description</p>
+            <textarea type="text" id="description" value={description} onChange={onchange} placeholder="Description" required className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600" />
+            <p className="text-lg mt-6 font-semibold">Offer</p>
+            <div className="flex mb-6">
+                <button type="button" id="offer" value={false}
+                    onClick={onchange} className={`mr-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${offer ? "bg-white text-black" : "bg-slate-600 text-white"}`}>
+                        yes
+                </button>
+                <button type="button" id="offer" value={true}
+                    onClick={onchange} className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${!offer ? "bg-white text-black" : "bg-slate-600 text-white"}`}>
+                        no
+                </button>
+            </div>
+            <div className="flex items-center mb-6">
+                <div>
+                    <p className="text-lg font-semibold">Regular Price</p>
+                    <div className="flex w-full justify-center items-center space-x-6">
+                        <input type="number" id="regularPrice" value={regularPrice} onChange={onChange} min="50" max="400000000" required className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"/>
+                    </div>
+                     {type === "rent" && ( 
+                      <div>
+                        <p className="text-md w-full whitespace-nowrap">$ / Month</p>
+                      </div>
+                     )}
+                </div>
+            </div>
+            {offer && (
+            <div className="flex items-center mb-6">
+                <div>
+                    <p className="text-lg font-semibold">Discounted Price</p>
+                    <div className="flex w-full justify-center items-center space-x-6">
+                        <input type="number" id="discountedPrice" value={discountedPrice} onChange={onChange} min="50" max="400000000" required={offer} className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center"/>
+                    </div>
+                    {type === "rent" && ( 
+                    <div>
+                        <p className="text-md w-full whitespace-nowrap">$ / Month</p>
+                    </div>
+                    )}
+                </div>
+            </div>
+            )}
+            <div className="mb-6">
+                <p className="text-lg font-semibold">Images</p>
+                <p className="text-gray-600">The first page will be the cover (max )</p>
+                <input type="file" id="images" onChange={onchange} accept=".jpg,.png,.jpeg" multiple required className="w-full px-3 py-1.5 text-gray-700 bg-white border border-gray-300 rounded transition duratio-150 ease-in-out focus:bg-white focus:border-slate-600"/>
+            </div>
+            <button type="submit" className="mb-6  w-full py-3 px-7 text-white bg-blue-600 font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700nhover:shadow-lg focus:bg-blue-700 focus:shadow-lg active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Create Listing</button>
         </form>
     </main>
   )
