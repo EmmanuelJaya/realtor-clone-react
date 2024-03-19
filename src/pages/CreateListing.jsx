b0e5ecb07b64e665d6d8607f0d462b00fe5c22b8
EmmanuelJaya <emmanuelsk04@gmail.com>
2024-03-19 19:07:29 +0530

    created latitude and longitude functionality

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
-import { onChange } from "react-toastify";
+import Spinner from "../components/Spinner";
+import {toast} from "react-toastify";
 
 export default function CreateListing() {
+    const [geoLocationEnabled, setGeoLocationEnabled] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [formData, setFormData] = useState({
         type: "rent",
         name: "",
@@ -14,8 +17,11 @@ export default function CreateListing() {
         offer: false,
         regularPrice: 0,
         discountedPrice: 0,
+        latitude: 0,
+        longitude: 0,
+        images: {}
     })
-    const {type, name, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountedPrice} = formData;
+    const {type, name, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountedPrice, latitude, longitude, images} = formData;
     function onChange (e) {
         let boolean = null;
         if(e.target.value === "true"){
@@ -37,10 +43,32 @@ export default function CreateListing() {
             }))
         }
     }
+    function onSubmit(e){
+        e.preventDefault();
+        setLoading(true);
+        if(discountedPrice >= regularPrice){
+            setLoading(false)
+            toast.error("Discounted price needs to be regular than the Regular price")
+            return;
+        }
+        if(images.length > 6){
+            setLoading(false)
+            toast.error("Maximum 6 images are only allowed");
+            return;
+        }
+        let geoLocationEnabled = {}
+        let location
+        if(geoLocationEnabled){
+            
+        }
+    }
+    if(loading){
+        return <Spinner />;
+    }
   return (
     <main className="max-w-md px-2 mx-auto">
         <h1 className="text-3xl text-center mt-6 font-bold">Create Listing</h1>
-        <form>
+        <form onSubmit={onSubmit}>
             <p className="text-lg mt-6 font-semibold">Sell / Rent</p>
             <div className="flex">
                 <button type="button" id="type" value="sale"
@@ -88,6 +116,18 @@ export default function CreateListing() {
             </div>
             <p className="text-lg font-semibold">Address</p>
             <textarea type="text" id="address" value={address} onChange={onChange} placeholder="Address" required className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600" />
+            {!geoLocationEnabled && (
+                <div className="flex space-x-6 justify-start mb-6">
+                    <div className="">
+                        <p className="text-lg font-semibold">Latitude</p>
+                        <input type="number" id="latitude" value={latitude} onChange={onChange} required min="-90" max="90" className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center"/>
+                    </div>
+                    <div className="">
+                        <p className="text-lg font-semibold">Longitude</p>
+                        <input type="number" id="longitude" value={longitude} onChange={onChange} required min="-180" max="180" className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center"/>
+                    </div>
+                </div>
+            )}
             <p className="text-lg font-semibold">Description</p>
             <textarea type="text" id="description" value={description} onChange={onChange} placeholder="Description" required className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600" />
             <p className="text-lg mt-6 font-semibold">Offer</p>
